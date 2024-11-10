ff1a22c85d9ce14cbcce3445887d5e5266463a8b
minobol <120034142+minobol@users.noreply.github.com>
2024-11-10 18:19:09 +0300

    Delete Image Feed directory

--- Image Feed/ImagesList/ImagesListCell.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-final class ImagesListCell: UITableViewCell {
-    @IBOutlet var imageViewList: UIImageView!
-    @IBOutlet var likeButtonList: UIButton!
-    @IBOutlet var labelList: UILabel!
-    static let reuseIdentifier = "ImagesListCell"
-}
