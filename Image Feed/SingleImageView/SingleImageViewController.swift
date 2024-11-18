1ed7be15eef32d10cb0bd5ba8cde01120eb8e5d2
minobol <migelnokia@gmail.com>
2024-11-19 00:50:38 +0300

    Navigation

---
+++ Image Feed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SingleImageViewController.swift
+//  Image Feed
+//
+//  Created by MacBook on 18.11.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+            }
+        }
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           imageView.image = image
+       }
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+}
