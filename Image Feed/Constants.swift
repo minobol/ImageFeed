22cd17c3e52710001be5c2e4147fe6c7d5a4ac8d
minobol <migelnokia@gmail.com>
2024-11-24 03:18:32 +0300

    no message

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Image Feed
+//
+//  Created by MacBook on 24.11.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "wsdzlJJAgmWg-HakW7f0Iyr-YOQCrSUTD_D6uiy7J9w"
+    static let secretKey = "yB_wxA1xYDHqZmlwxRI854HpOYrNG-nrOAsvkpwfgrI"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com" )!
+}
